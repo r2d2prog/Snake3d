1f358da0f207c121983b32e422ec970688c79384
Nikolai Kochuk <ganortosys@gamil.com>
2022-12-25 14:09:49 +0500

    Add snake module

---
+++ snake.h
@@ -0,0 +1,12 @@
+#ifndef SNAKE_H_INCLUDED
+#define SNAKE_H_INCLUDED
+class Snake
+{
+private:
+    unsigned int snakeVao;
+    unsigned int snakeVbo;
+public:
+    Snake();
+    ~Snake();
+};
+#endif // SNAKE_H_INCLUDED
